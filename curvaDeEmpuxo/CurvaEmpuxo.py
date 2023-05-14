620ec085b4d0d7c0a1bce4cbc4e2701342338f7f
Matheus Parrela <92748188matheusparrela@users.noreply.github.com>
2023-05-14 17:32:52 -0300

    Classe com os métodos para cálcular a curva de empuxo

---
+++ curvaDeEmpuxo/CurvaEmpuxo.py
@@ -0,0 +1,22 @@
+
+class CurvaEmpuxo:
+    def __init__(self, file):
+        self.file = file
+
+    def impulso_total(self):
+        return 0
+
+    def empuxo_max(self):
+        return 0
+
+    def empuxo_med(self):
+        return 0
+
+    def velociadade_media_gases(self):
+        return 0
+
+    def impulso_especifico_med(self):
+        return 0
+
+    def fluxo_massa_med(self):
+        return 0
