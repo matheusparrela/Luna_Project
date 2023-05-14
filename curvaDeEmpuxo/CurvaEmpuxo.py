4193d011e6c24329e0bf0e92777697c445b135ba
Matheus Parrela <92748188matheusparrela@users.noreply.github.com>
2023-05-14 19:23:02 -0300

    Método impulso total atualizado.

--- curvaDeEmpuxo/CurvaEmpuxo.py
+++ curvaDeEmpuxo/CurvaEmpuxo.py
@@ -1,11 +1,35 @@
+import pandas as pd
+import numpy as np
 
 class CurvaEmpuxo:
+
     def __init__(self, file):
-        self.file = file
+        self.file1 = pd.read_table(file)
+        self.file = np.loadtxt(file, dtype=float)
+
+    '''
+    def refina_dados(self):
+        return 0
+    '''
 
     def impulso_total(self):
-        return 0
 
+        x = np.round(self.file[0:-1, 0], decimals=4)
+        y = np.round(self.file[0:-1, 1], decimals=4)
+        h = (x[-1] - x[0])/len(x)
+        print(np.round(h, decimals=4))
+
+        n = 0
+        soma = 0
+        lim = int(len(y)/3)*3
+
+        while n < lim:
+            soma1 = 3*h/8 * (y[n] + 3 * y[n+1] + 3 * y[n + 3] + y[n + 4])
+            soma = soma + soma1
+            n = n + 3
+        return soma
+
+    '''
     def empuxo_max(self):
         return 0
 
@@ -20,3 +44,4 @@ class CurvaEmpuxo:
 
     def fluxo_massa_med(self):
         return 0
+    '''
