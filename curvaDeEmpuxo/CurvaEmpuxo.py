93b6e5567f4dbad5b74117986ea42ca1764e076f
Matheus Parrela <92748188matheusparrela@users.noreply.github.com>
2023-05-15 01:06:35 -0300

    Add os metodos de empuxo maximo e medio, impulso especifico médio, velocidade média dos gases e fluxo de massa médio

--- curvaDeEmpuxo/CurvaEmpuxo.py
+++ curvaDeEmpuxo/CurvaEmpuxo.py
@@ -3,47 +3,44 @@ import matplotlib.pyplot as plt
 
 class CurvaEmpuxo:
 
-    def __init__(self, file, min):
-        self.file = np.loadtxt(file, dtype=float)
+    def __init__(self, file, min, prop_massa):
+        self.file = np.loadtxt(file, dtype=float) # Arquivo de dados
         self.min_empuxo = min  # Empuxo mínimo para definir o início e fim da curva (N)
+        self.prop_massa = prop_massa # Massa de propelente (kg)
+        self.x = np.round(self.file[:, 0], decimals=4)
+        self.y = np.round(self.file[:, 1], decimals=4)
+        self.GRAVIDADE = 9.80665
 
-
-    def refina_dados(self, x, y):
-        tam = len(x)-1
+    def refina_dados(self):
+        tam = len(self.x)-1
         for i in range(tam, -1, -1):
-            if y[i] < 0.05:
-                x = np.delete(x, i)
-                y = np.delete(y, i)
-        return x, y
-
+            if self.y[i] < 0.05:
+                self.x = np.delete(self.x, i)
+                self.y = np.delete(self.y, i)
 
     def impulso_total(self):
 
-        x = np.round(self.file[:, 0], decimals=4)
-        y = np.round(self.file[:, 1], decimals=4)
+        self.refina_dados()
 
-        x, y = self.refina_dados(x, y)
+        while (len(self.x)-1) % 3 != 0:
+            self.x = np.delete(self.x, -1)
+            self.y = np.delete(self.y, -1)
 
-        while (len(x)-1) % 3 != 0:
-            x = np.delete(x, -1)
-            y = np.delete(y, -1)
-
-        h = (x[-1] - x[0]) / (len(x)-1)
+        h = (self.x[-1] - self.x[0]) / (len(self.x)-1)
         n = 0
         soma = 0
 
-        while n < len(x)-3:
-            soma1 = 3*h/8 * (y[n] + 3*y[n+1] + 3*y[n+2] + y[n+3])
+        while n < len(self.x)-3:
+            soma1 = 3*h/8 * (self.y[n] + 3*self.y[n+1] + 3*self.y[n+2] + self.y[n+3])
             soma = soma + soma1
             n = n + 3
 
-        self.grafico(x, y)
+        self.grafico()
         return soma
 
+    def grafico(self):
 
-    def grafico(self, x, y):
-
-        plt.plot(x, y)
+        plt.plot(self.x, self.y)
 
         plt.xlabel('Tempo (s)')
         plt.ylabel('Eixo Y')
@@ -51,20 +48,17 @@ class CurvaEmpuxo:
 
         plt.savefig('grafico.png')
 
-
-    '''
     def empuxo_max(self):
-        return 0
+        return max(self.y)
 
     def empuxo_med(self):
-        return 0
+        return self.impulso_total()/max(self.x)
 
     def velociadade_media_gases(self):
-        return 0
+        return self.impulso_total()/self.prop_massa
 
     def impulso_especifico_med(self):
-        return 0
+        return self.impulso_total()/(self.prop_massa*self.GRAVIDADE)
 
     def fluxo_massa_med(self):
-        return 0
-    '''
+        return self.prop_massa/max(self.x)
--- curvaDeEmpuxo/main.py
+++ curvaDeEmpuxo/main.py
@@ -2,6 +2,13 @@ import CurvaEmpuxo as ce
 
 file = 'teste.txt'  # Arquivo de dados
 min = 0.05          # Empuxo mínimo para definir o início e fim da curva (N)
+massa = 0.002542    # Massa de propelente (kg)
 
-empuxo = ce.CurvaEmpuxo(file, min)
-print('Impulso Total:', empuxo.impulso_total())
+empuxo = ce.CurvaEmpuxo(file, min, massa)
+
+print('Impulso Total:', empuxo.impulso_total(), 'N*s')
+print('Empuxo Máximo:', empuxo.empuxo_max(), 'N')
+print('Empuxo Médio:', empuxo.empuxo_med(), 'N')
+print('Velocidade Média de ejeção dos gases:', empuxo.velociadade_media_gases(), 'm/s')
+print('Impulso Específico Médio:', empuxo.impulso_especifico_med())
+print('Fluxo de Massa Médio:', empuxo.fluxo_massa_med())
